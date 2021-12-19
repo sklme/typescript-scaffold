b361d0cb27ea67da87972646162050908e04bcc2
skl <sun.keliang@foxmail.com>
2021-12-19 23:37:13 +0800

    bon

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import { program } from "commander";
-
+// import { program } from "commander";
 console.log(121);
+
+function xx(s: string) {
+  return "s" + s;
+}
+
+export function xx1(xx: string) {
+  return xx + "123";
+}
+
+export default xx;
