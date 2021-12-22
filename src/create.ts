110125b2274ba8d648220719c515ff884adf2979
wrynnsun <wrynnsun@tencent.com>
2021-12-22 21:15:53 +0800

    feat(create): 命令初始化

---
+++ src/create.ts
@@ -0,0 +1,3 @@
+export default function createNewApp(name: string) {
+  console.log("你建立的项目的命中是", name);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-// import { program } from "commander";
-console.log(121);
+import { program } from "commander";
+import createNewApp from "./create";
+// import pjson from "../package.json";
+// console.log(pjson.version);
 
-function xx(s: string) {
-  return "s" + s;
-}
-
-export function xx1(xx: string) {
-  return xx + "12345";
-}
-
-export default xx;
+program
+  .version("1.0.")
+  .command("create <appName>")
+  .description("创建新的项目")
+  .action((name: string) => {
+    createNewApp(name);
+  })
+  .parse();
