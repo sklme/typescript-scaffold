ec9dd3775e9e52775afe534e2ad9c1b6cbd20117
wrynnsun <wrynnsun@tencent.com>
2021-12-23 21:01:21 +0800

    feat: 检查路径是否已经被占用

--- src/create.ts
+++ src/create.ts
@@ -1,9 +1,22 @@
+import { resolve } from "path";
+import { existsSync } from "fs";
 import { listColors } from "./util/colorText";
 
 listColors();
 
 export default function createNewApp(name: string) {
   console.log("项目名字是", name);
+
+  // 检查目录是否存在
+  checkDirIsExist(name);
 }
 
 // 检查文件路径是否已经存在
+function checkDirIsExist(appName: string) {
+  const fullPath = resolve(process.cwd(), appName);
+  if (existsSync(fullPath)) {
+    console.log(`${fullPath} 目录已经存在`);
+    process.exit(1);
+  }
+  console.log(fullPath);
+}
